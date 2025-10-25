88217ee38e7bf15bff6d43d570e5ad400e9a9014
Kaziyel-Islam <mdkaziyelislam@gmail.com>
2025-10-25 16:07:00 +0600

    enquee style add

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html lang="<?php language_attributes(); ?>">
+    
+<head>
+
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <title><?php bloginfo('name'); ?> - <?php bloginfo('description'); ?> </title>
+
+    <?php wp_head(); ?>
+
+</head>
+
+<body <?php body_class(); ?> >
+    
+<header>
+
+
+</header>
